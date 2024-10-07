2d903b9c64a27e410ceae134c91d9a5c65258459
Demi Lune <anshikathakur596@gmail.com>
2024-10-07 15:27:01 +0530

    Create main.ts

---
+++ task-manager-app/src/main.ts
@@ -0,0 +1,5 @@
+// main.ts
+import { Task } from './task'; // Importing the Task class
+
+const task1 = new Task("Learn TypeScript", false);
+task1.displayTask(); // Output: Learn TypeScript is not completed.
