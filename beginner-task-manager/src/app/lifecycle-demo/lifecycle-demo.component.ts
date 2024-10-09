dafb671b173d8a4a0e2d705f33f35eb09afba2e9
Demi Lune <anshikathakur596@gmail.com>
2024-10-09 14:25:12 +0530

    Update lifecycle-demo.component.ts

--- beginner-task-manager/src/app/lifecycle-demo/lifecycle-demo.component.ts
+++ beginner-task-manager/src/app/lifecycle-demo/lifecycle-demo.component.ts
@@ -14,6 +14,7 @@ import { Component, OnInit, OnDestroy, AfterViewInit } from '@angular/core';
 })
 export class LifecycleDemoComponent implements OnInit, OnDestroy, AfterViewInit {
   
+  // Called once when the component is created
   constructor() {
     console.log('LifecycleDemoComponent Constructor');
   }
